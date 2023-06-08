ea08d4532e9e6d9140b337a92caf90f0575f5186
Yurii Yurents <yurii.yurenets-netconomy@dglecom.onmicrosoft.com>
2023-06-08 16:44:52 +0300

    fix

--- src/components/select/select.js
+++ src/components/select/select.js
@@ -2,13 +2,50 @@ import React from 'react';
 import Select from 'react-select';
 import s from '../Inputs/input.module.css';
 
+const customStyles = {
+  container: provided => ({
+    ...provided,
+  }),
+  control: (provided, state) => ({
+    ...provided,
+    backgroundColor: '#ffffff',
+    border: state.isFocused ? '2px solid #e064a0' : '2px solid #475467',
+    boxShadow: state.isFocused ? '0 0 0 1px #e064a0' : 'none',
+    '&:hover': {
+      border: '2px solid #e064a0',
+      boxShadow: '0 0 0 1px #e064a0',
+      color: '#e064a0',
+    },
+    borderRadius: '8px',
+    minHeight: '44px',
+  }),
+  option: (provided, state) => ({
+    ...provided,
+    backgroundColor: state.isSelected ? '#007bff' : 'white',
+    color: state.isSelected ? 'white' : 'black',
+  }),
+  dropdownIndicator: (provided, state) => ({
+    ...provided,
+    color: state.isFocused ? '#e064a0' : '',
+    '&:hover': {
+      color: '#e064a0',
+    },
+  }),
+  multiValue: provided => ({
+    ...provided,
+    backgroundColor: '#ffffff',
+    color: '#344054',
+    border: '1px solid #D0D5DD',
+    borderRadius: '6px',
+  }),
+};
+
 const CustomSelect = ({
   options,
   isMulti,
   onChange,
   value,
   placeholder,
-  customStyles,
   label,
 }) => {
   return (
